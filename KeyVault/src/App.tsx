422bd2c42d36a26d209c98d0d449f30533c4c1fb
Andrei Bakos <andreib347@yahoo.com>
2023-01-20 20:22:54 +0200

    WIP + Added form + some styling

--- KeyVault/src/App.tsx
+++ KeyVault/src/App.tsx
@@ -1,32 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+const handleSubmit = () => {
+  alert(`email: ${email} \n password: ${password}`)
+}
+
+  useEffect(() => {
+      //fetch data from backend here
+  },[])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App" style={{ display: 'grid', placeItems: 'center', height: screen.height / 1.6}}>
+      <h1 className='header-text'>Welcome to Key Vault!</h1>
+      <div className='form-container' style={{ width: screen.width / 2.5 }}>
+        <div className='form-group'>
+          <label> Email:</label>
+          <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className='form-group'>
+          <label> Password:</label>
+          <input className="form-control" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div style={{ display: 'flex', color: 'black', alignSelf: 'center' }}>
+          <p>Don't have an account?</p>
+          <button className='sign-up-button' onClick={() => alert(2)}>Sign Up</button>
+        </div>
+        <button className='submit-btn' onClick={handleSubmit}>Login</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
